e1045c3d3b998bad943ac390c1ea531f809f3b98
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-06-14 14:32:44 -0400

    commit tree 7ab7e87a3615c0cd7dd1c660c9a908219146be81

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
@@ -6,16 +6,12 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
+    fetch('https://dog.ceo/api/breeds/image/random/3')
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.messages);
       });
-  });
-
-  console.log("render");
+  }, []);
 
   return (
     <div>
@@ -25,5 +21,4 @@ function DogPics() {
     </div>
   );
 }
-
 export default DogPics;
